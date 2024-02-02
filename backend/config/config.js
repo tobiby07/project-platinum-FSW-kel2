f489890cae6528b39d8c8d6f97eeb4e2350e54cb
lolin <putu.lolin@gmail.com>
2024-02-02 11:19:38 +0700

    setup configuration file

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
+module.exports =
 {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "development": {
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_NAME_TEST,
+      "host": process.env.DB_HOST,
+      "dialect": process.env.DB_DIALECT
+    },
   },
   "test": {
     "username": "root",
