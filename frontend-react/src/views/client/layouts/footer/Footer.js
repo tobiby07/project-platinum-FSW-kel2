76a630cb52c4692fdbf3bfab3819d060ca2c4e33
Ical <choco.cheese@protonmail.com>
2024-02-23 22:17:20 +0700

    [Fix] Mengganti class menjadi className di Footer.js

--- frontend-react/src/views/client/layouts/footer/Footer.js
+++ frontend-react/src/views/client/layouts/footer/Footer.js
@@ -12,11 +12,11 @@ function Footer() {
     <footer>
 
         { /* Widgets */ }
-        <div class="bg-light py-3 py-xl-5 mt-4">
-            <div class="container">
-                <div class="row gy-3 align-items-sm-center">
-                    <div class="col-xs-12 col-sm-6 col-xl-3 order-0 order-xl-0">
-                        <div class="footer-logo-wrapper text-center text-sm-start">
+        <div className="bg-light py-3 py-xl-5 mt-4">
+            <div className="container">
+                <div className="row gy-3 align-items-sm-center">
+                    <div className="col-xs-12 col-sm-6 col-xl-3 order-0 order-xl-0">
+                        <div className="footer-logo-wrapper text-center text-sm-start">
                             <a href="/home">
                             <img
                                 src={logo}
@@ -27,37 +27,37 @@ function Footer() {
                         </div>
                     </div>
 
-                    <div class="col-xs-12 col-xl-6 order-2 order-xl-1">
-                        <ul class="nav justify-content-center">
-                            <li class="nav-item">
-                                <a class="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">About</a>
+                    <div className="col-xs-12 col-xl-6 order-2 order-xl-1">
+                        <ul className="nav justify-content-center">
+                            <li className="nav-item">
+                                <a className="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">About</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">Contact</a>
+                            <li className="nav-item">
+                                <a className="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">Contact</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">Terms</a>
+                            <li className="nav-item">
+                                <a className="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">Terms</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">Privacy</a>
+                            <li className="nav-item">
+                                <a className="nav-link link-secondary px-2 px-md-3 fw-medium" href="#!">Privacy</a>
                             </li>
                         </ul>
                     </div>
 
-                    <div class="col-xs-12 col-sm-6 col-xl-3 order-1 order-xl-2">
-                        <ul class="nav justify-content-center justify-content-sm-end">
-                            <li class="nav-item">
-                                <a class="nav-link link-dark px-3" href="https://www.facebook.com">
+                    <div className="col-xs-12 col-sm-6 col-xl-3 order-1 order-xl-2">
+                        <ul className="nav justify-content-center justify-content-sm-end">
+                            <li className="nav-item">
+                                <a className="nav-link link-dark px-3" href="https://www.facebook.com">
                                     <FaFacebook size={24} />
                                 </a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link link-dark px-3" href="https://www.instagram.com">
+                            <li className="nav-item">
+                                <a className="nav-link link-dark px-3" href="https://www.instagram.com">
                                     <FaInstagram size={24} />
                                 </a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link link-dark px-3" href="https://twitter.com">
+                            <li className="nav-item">
+                                <a className="nav-link link-dark px-3" href="https://twitter.com">
                                     <FaXTwitter size={24} />
                                 </a>
                             </li>
@@ -68,11 +68,11 @@ function Footer() {
         </div>
 
         { /* Credit */ }
-        <div class="bg-dark py-3">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <div class="credits text-white text-center">
+        <div className="bg-dark py-3">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <div className="credits text-white text-center">
                         (2024) FSW 39 - Kelompok 2
                         </div>
                     </div>
