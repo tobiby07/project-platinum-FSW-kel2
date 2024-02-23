b0b33c94e7e1c991bfd8e4582224b9b4e6266e02
Ical <choco.cheese@protonmail.com>
2024-02-23 14:14:57 +0700

    Menambah Footer

--- frontend-react/src/views/client/HomePage.js
+++ frontend-react/src/views/client/HomePage.js
@@ -1,8 +1,11 @@
 import HeaderHome from "./layouts/Header/HeaderHome"
+import Footer from "./layouts/footer/Footer"
+
 const HomePage = ()=> {
     return(
         <div>
             <HeaderHome/>
+            <Footer/>
         </div>
     )
 }
