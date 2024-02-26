622af5e3f1e2ae90aae055844b9bb0527bf02f07
Ical <choco.cheese@protonmail.com>
2024-02-26 11:07:46 +0700

    Memberikan styling pada UserDetail.jsx

--- frontend-react/src/views/client/layouts/main/UserDetail.jsx
+++ frontend-react/src/views/client/layouts/main/UserDetail.jsx
@@ -1,69 +1,107 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Modal } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Modal } from "react-bootstrap";
 import EditUser from "./component/EditUser";
-import Button from '../../../auth/components/button';
+import Button from "../../../auth/components/button";
+import profilePic from "../../../image/profile-pic.png";
 
 const UserDetail = () => {
-    const [userData, setUserData] = useState(null);
-    const [showModal, setShowModal] = useState(false); 
+  const [userData, setUserData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const idUser = localStorage.getItem('id');
-                const response = await axios.get(`http://localhost:3001/api/users/${idUser}`);
-                setUserData(response.data); 
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            }
-        };
-
-        fetchData(); 
-    }, []); 
-
-    const openModal = () => {
-        setShowModal(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const idUser = localStorage.getItem("id");
+        const response = await axios.get(
+          `http://localhost:3001/api/users/${idUser}`
+        );
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
     };
 
-    const closeModal = () => {
-        setShowModal(false);
-    };
+    fetchData();
+  }, []);
 
-    return (
-        <div className="">
-            {userData ? (
-                <div>
-                    <h2>User Detail</h2>
-                    <p>Name: {userData.name}</p>
-                    <p>Email: {userData.email}</p>
-                    <p>Phone Number: {userData.phoneNumber}</p>
-                    {userData.Address && (
-                        <div>
-                            <p>Province: {userData.Address.province}</p>
-                            <p>Regency: {userData.Address.regency}</p>
-                            <p>District: {userData.Address.district}</p>
-                            <p>Village: {userData.Address.village}</p>
-                        </div>
-                    )}
-                    <button onClick={openModal}>Edit User</button>
-                    <Modal show={showModal} onHide={closeModal} size='lg' >
-                        <Modal.Header closeButton>
-                            <Modal.Title>Edit User</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body style={{ maxHeight: '800vh' }} >
-                            <EditUser userData={userData} />
-                        </Modal.Body>
-                        <Modal.Footer>
-                            <Button onClick={closeModal} conten={"Close"} />
-                        </Modal.Footer>
-                    </Modal>
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
+    <div>
+      {userData ? (
+        <div className="user-detail">
+          <h2 className="mt-4 text-center fw-semibold">User Detail</h2>
+          <div
+            className="card my-4 mx-auto rounded-0"
+            style={{ maxWidth: 540 }}
+          >
+            <div className="row g-0">
+              <div className="col-md-5">
+                <img
+                  src={profilePic}
+                  className="img-fluid rounded-0 p-md-4"
+                  alt="profile"
+                />
+              </div>
+              <div className="col-md-7">
+                <div className="card-body">
+                  <h5 className="card-title fw-semibold">Nama</h5>
+                  <p className="card-text">{userData.name}</p>
+                  <h5 className="card-title fw-semibold">Email</h5>
+                  <p className="card-text">{userData.email}</p>
+                  <h5 className="card-title fw-semibold">Phone Number</h5>
+                  <p className="card-text">{userData.phoneNumber}</p>
+
+                  {userData.Address && (
+                    <div className="mb-1">
+                      <h5 className="card-title fw-semibold">Province</h5>
+                      <p className="card-text">{userData.Address.province}</p>
+                      <h5 className="card-title fw-semibold">Regency</h5>
+                      <p className="card-text">{userData.Address.regency}</p>
+                      <h5 className="card-title fw-semibold">District</h5>
+                      <p className="card-text">{userData.Address.district}</p>
+                      <h5 className="card-title fw-semibold">Village</h5>
+                      <p className="card-text">{userData.Address.village}</p>
+                    </div>
+                  )}
                 </div>
-            ) : (
-                <p>Loading...</p>
-            )}
+              </div>
+            </div>
+            <div className="card-footer text-body-secondary">
+              <div className="py-2 px-0 text-end">
+                <button
+                  className="btn bg-dark text-white rounded-0"
+                  onClick={openModal}
+                >
+                  Edit User
+                </button>
+                <Modal show={showModal} onHide={closeModal} size="lg">
+                  <Modal.Header closeButton>
+                    <Modal.Title>Edit User</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body style={{ maxHeight: "800vh" }}>
+                    <EditUser userData={userData} />
+                  </Modal.Body>
+                  <Modal.Footer>
+                    <Button onClick={closeModal} conten={"Close"} />
+                  </Modal.Footer>
+                </Modal>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default UserDetail;
