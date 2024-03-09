b21dde5f48fc32937e81af53ab0b7be8e7d144fa
lolin <putu.lolin@gmail.com>
2024-03-09 18:51:35 +0800

    update swagger order-patch

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -74,8 +74,9 @@ const OrderController = {
 
   // Update an existing Order
   updateOrder: async (req, res) => {
-    const { id } = req.params;
+    const id = req.params.id;
     const { userId, totalPrice, shippingAddress, orderStatus, orderDetails } = req.body;
+    console.log(userId, totalPrice, shippingAddress, orderStatus, orderDetails)
     try {
       const orderUpdate = await Order.findByPk(id);
       if (!orderUpdate) {
